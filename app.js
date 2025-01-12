789a9fdb6235472e1742946c8cfe38496a765710
Flamez001 <flameztech001@gmail.com>
2025-01-12 15:58:09 +0100

    Yay

---
+++ app.js
@@ -0,0 +1,43 @@
+// VARIABLES
+let name = 'Peculiar';
+let whatIwantToBecomeInLife = 'Programmer';
+let gender = 'Male';
+let twitterHandle = 'codewithflamez';
+
+console.log(name);
+console.log(whatIwantToBecomeInLife);
+console.log(gender);
+console.log(twitterHandle);
+
+console.table({ Fullname: name, Ambition: whatIwantToBecomeInLife, Sex: gender, Socials: twitterHandle });
+
+// BASIC MATHEMATICS
+let firstFavNum = 29;
+let secondFavNum = 01;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
+
+let Money = 50;
+let MoneySpent = 50;
+console.log(Money + MoneySpent);
+
+MoneySpent = 30;
+console.log(Money + MoneySpent);
+
+Money = 20;
+MoneySpent = 20;
+console.log(Money + MoneySpent);
+
+console.log(Boolean(3 - 9));
+
+let notSure = undefined;
+console.log(notSure);
+
+notSure = 500 + 500;
+console.log(notSure);
+
